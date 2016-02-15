b7aa15ce496d7da802c0413e0586e5012f3354a7
Juan Manuel Tastzian <jm@tast.com.ar>
2016-02-15 14:48:35 -0300

    add captcha validation and error message show

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -31,13 +31,17 @@ public class Application extends Controller {
 		render(post, randomID);
 	}
 
-	public static void postComment(Long postId, @Required String author, @Required String content) {
+	public static void postComment(Long postId, @Required(message = "Author is required") String author,
+			@Required(message = "A message is required") String content,
+			@Required(message = "Please type the code") String code, String randomID) {
 		Post post = Post.findById(postId);
+		validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again");
 		if (validation.hasErrors()) {
-			render("Application/show.html", post);
+			render("Application/show.html", post, randomID);
 		}
 		post.addComment(author, content);
 		flash.success("Thanks for posting %s", author);
+		Cache.delete(randomID);
 		show(postId);
 	}
 
