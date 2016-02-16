0cf9ff63fae81540a153c3254afcfda6a01f218a
Juan Manuel Tastzian <jm@tast.com.ar>
2016-02-16 14:17:54 -0300

    add CRUD models, validation and label strings

--- app/models/Post.java
+++ app/models/Post.java
@@ -13,17 +13,24 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import play.data.validation.MaxSize;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 
 @Entity
 public class Post extends Model {
 
+	@Required
 	public String title;
+	@Required
 	public Date postedAt;
 
 	@Lob
+	@Required
+	@MaxSize(10000)
 	public String content;
 
+	@Required
 	@ManyToOne
 	public User author;
 
@@ -70,6 +77,10 @@ public class Post extends Model {
 		return Post
 				.find("select distinct p from Post p join p.tags as t where t.name in (:tags) group by p.id, p.author, p.title, p.content,p.postedAt having count(t.id) = :size")
 				.bind("tags", tags).bind("size", tags.length).fetch();
+	}
+
+	public String toString() {
+		return title;
 	}
 
 }
--- app/models/User.java
+++ app/models/User.java
@@ -2,12 +2,18 @@ package models;
 
 import javax.persistence.Entity;
 
+import play.data.validation.Email;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 
 @Entity
 public class User extends Model {
 
+	@Email
+	@Required
 	public String email;
+
+	@Required
 	public String password;
 	public String fullname;
 	public boolean isAdmin;
@@ -20,6 +26,10 @@ public class User extends Model {
 
 	public static User connect(String email, String password) {
 		return find("byEmailAndPassword", email, password).first();
+	}
+
+	public String toString() {
+		return email;
 	}
 
 }
