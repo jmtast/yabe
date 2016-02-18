83b294d6b3aab727a7fc96cef3825ab253aeeda3
Juan Manuel Tastzian <jm@tast.com.ar>
2016-02-18 11:10:04 -0300

    add selenium tests for home and posting a new comment

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -35,7 +35,9 @@ public class Application extends Controller {
 			@Required(message = "A message is required") String content,
 			@Required(message = "Please type the code") String code, String randomID) {
 		Post post = Post.findById(postId);
-		validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again");
+		if (!Play.id.equals("test")) {
+			validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again");
+		}
 		if (validation.hasErrors()) {
 			render("Application/show.html", post, randomID);
 		}
